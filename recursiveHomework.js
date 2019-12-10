25abf9e7f621e0f6d0d9f6e2bdc49b3302060447
gilbertjusmc <jgilbert1341@gmail.com>
2019-12-09 19:42:52 -0500

    finished recursion HW

--- recursiveHomework.js
+++ recursiveHomework.js
@@ -5,3 +5,21 @@ First, find the closed form solution of this, and code a function.
 Then, write the recursive function of this.
 Think about the factorial method we went over.
 */
+
+function addClosedForm(n) {
+    // 5 * (5 + 1) / 2 = 15
+    // 5 * 6 / 2 = 15
+    // 30 / 2 = 15
+
+    return n * (n + 1) / 2;
+}
+
+
+function addRecursively(n) {
+    if (n < 0) return 0;
+    if (n === 0) return 0;
+    return n + addRecursively(n - 1);
+}
+
+console.log('Closed Form Answer: ' + addClosedForm(6));
+console.log('Recursive Answer: ' + addRecursively(6));
